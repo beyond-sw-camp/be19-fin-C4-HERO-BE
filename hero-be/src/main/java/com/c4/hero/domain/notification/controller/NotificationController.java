8624c7264f41158c8e4b8c2af82d8dcfecb92574
haenin <heyone0022@gmail.com>
2025-12-09 18:18:54 +0900

    [Feat] WebSocket 기반 실시간 알림 메시지 전송 기능 구현 초기 설계

--- hero-be/src/main/java/com/c4/hero/domain/notification/controller/NotificationController.java
+++ hero-be/src/main/java/com/c4/hero/domain/notification/controller/NotificationController.java
@@ -1,7 +1,18 @@
 package com.c4.hero.domain.notification.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Slf4j
+@Controller
 public class NotificationController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/notifications")
+    public String sendMessage(String message){
+        log.info("Received message: {}", message);
+        return message;
+    }
 }
