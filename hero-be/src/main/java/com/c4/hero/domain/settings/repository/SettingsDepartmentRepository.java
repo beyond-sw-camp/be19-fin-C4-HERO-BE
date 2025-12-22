a3a8ee793e0a2c86f1c8fd24c9bc1fff3ef98a4f
bynmch <byunmch@naver.com>
2025-12-22 14:24:23 +0900

    [Fix] 부서목록조회 쿼리메서드 수정

--- hero-be/src/main/java/com/c4/hero/domain/settings/repository/SettingsDepartmentRepository.java
+++ hero-be/src/main/java/com/c4/hero/domain/settings/repository/SettingsDepartmentRepository.java
@@ -36,5 +36,11 @@ public interface SettingsDepartmentRepository extends JpaRepository<SettingsDepa
 	 */
 	List<SettingsDepartment> findAllByDepartmentIdNotIn(List<Integer> departmentIds);
 
-    List<SettingsDepartment> findByDepartmentIdGreaterThan(int attr0);
+    /**
+     * 부서 ID 0 이상 부서목록 조회
+     *
+     * @param adminId
+     * @return  부서 목록
+     */
+    List<SettingsDepartment> findByDepartmentIdGreaterThan(int adminId);
 }
