c782032134986a3b0b76b905e0508eb995758f0d
haenin <gpdnjsqhdld@naver.com>
2026-01-07 17:37:33 +0900

    [Fix] 배포를 위한 수정

--- hero-be/src/main/java/com/c4/hero/common/config/SecurityConfig.java
+++ hero-be/src/main/java/com/c4/hero/common/config/SecurityConfig.java
@@ -215,12 +215,6 @@ public class SecurityConfig {
         // 허용할 Origin (프론트엔드 주소)
         configuration.setAllowedOriginPatterns(Arrays.asList("*"));
 
-        configuration.setAllowedOrigins(Arrays.asList(
-
-                "https://hero-hr.site",
-                "http://localhost:8080" // 개발환경
-        ));
-
         // 허용할 HTTP 메서드
         configuration.setAllowedMethods(Arrays.asList(
                 "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"
